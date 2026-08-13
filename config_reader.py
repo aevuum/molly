550c756f3b9727a33f275221706fb67473a359a1
aev3m <waifu2507@gmail.com>
2026-08-13 22:58:21 +0300

    Update config_reader.py

--- config_reader.py
+++ config_reader.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
   TOKEN: SecretStr
+  postgres_db: str
+  postgres_user: str
+  postgres_password: SecretStr
+  postgres_host: str
+  postgres_port: int
+  database_url: str
   model_config: SettingsConfigDict = SettingsConfigDict(
     env_file='.env',
     env_file_encoding='utf-8'
